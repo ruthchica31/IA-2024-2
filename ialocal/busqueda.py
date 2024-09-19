afceefd858f69f087ac356640ce0da4f9af677d6
ruthchica31 <cgre1351053978@gmail.com>
2024-09-19 10:13:10 -0500

    practicas con python

---
+++ ialocal/busqueda.py
@@ -0,0 +1,13 @@
+mi lista [3, 7, 4, 9, 21
+
+elemento a buscar = 4
+
+resultado busqueda lineal(mi lista, elemento a buscar)
+
+if resultado != -1:
+
+print("El elemento", elemento a buscar, "se encuentra en el indice", resultado)
+
+else:
+
+print("El elemento", elemento a buscar, "no se encontra en la lista")
---
+++ ialocal/queuepractice.py
@@ -0,0 +1,16 @@
+containerFila=[]
+
+containerFila.append(12)
+
+
+
+
+for i in containerFila:
+    print(i)
+
+print("after pop")
+
+for i in containerFila:
+    print(i)
+
+
